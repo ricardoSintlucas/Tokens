65f809bf10c43a61e6a8187b04a620a7baba87f2
Ricardo Creemers <127936801+ricardoSintlucas@users.noreply.github.com>
2025-04-24 12:09:47 +0200

    add and remove tokens

--- sketch.js
+++ sketch.js
@@ -1,37 +1,63 @@
 let tokens = [];
 let amountTokens = 5;
-function setup() 
-{
-	createCanvas(windowWidth, windowHeight);
-    background(255);
-    
-    for(let i = 0; i < amountTokens; i++){
-        let token = new Token(random(width), random(height));
-        let tokenSize = token.outerRingSize+token.outerRingStrokeWeight;
-        let centerX = (tokenSize*amountTokens)/2;
-        let x = centerX+((tokenSize*i)+tokenSize/2);
-        let y = 100;
-        tokens.push(new Token(x, y));
-    }
-    for(let i = 0; i < tokens.length; i++){
-        tokens[i].draw();
-    }
-       
+function setup() {
+  createCanvas(1200, 300);
+  background(43, 45, 57);
+
+  for (let i = 0; i < amountTokens; i++) {
+    let token = new Token(random(width), random(height));
+    let tokenSize = token.outerRingSize + token.outerRingStrokeWeight;
+    let centerX = 0;
+    let x = centerX + (tokenSize * i + tokenSize / 2);
+    let y = 100;
+    tokens.push(new Token(x, y));
+  }
+
+  for (let i = 0; i < tokens.length; i++) {
+    tokens[i].draw();
+  }
+
+  let addButton = createButton("add token");
+  addButton.mousePressed(addToken);
+  let removeButton = createButton("remove token");
+  removeButton.mousePressed(removeToken);
 }
 
-
-function mouseClicked(){
-    for(let i = 0; i < tokens.length; i++){
-        let token = tokens[i];
-        let distance = dist(mouseX, mouseY, token.x, token.y);
-        if(distance < token.size/2+token.borderSize/2){
-            //console.log("Clicked inside inner circle");
-            token.flipInnerRing();
-        }else if(distance < token.outerRingSize/2+token.outerRingStrokeWeight/2){
-            // console.log("Clicked inside outer ring");
-            token.flipOuterRing();
-        }
+function mouseClicked() {
+  for (let i = 0; i < tokens.length; i++) {
+    let token = tokens[i];
+    let distance = dist(mouseX, mouseY, token.x, token.y);
+    if (distance < token.size / 2 + token.borderSize / 2) {
+      //console.log("Clicked inside inner circle");
+      token.flipInnerRing();
+    } else if (distance < token.outerRingSize / 2 + token.outerRingStrokeWeight / 2) {
+      // console.log("Clicked inside outer ring");
+      token.flipOuterRing();
     }
-    
-    
+  }
+}
+
+function addToken() {
+  background(43, 45, 57);
+  let token = new Token(random(width), random(height));
+  let tokenSize = token.outerRingSize + token.outerRingStrokeWeight;
+  let centerX = 0;
+  let x = centerX + (tokenSize * tokens.length + tokenSize / 2);
+  let y = 100;
+  tokens.push(new Token(x, y));
+  for (let i = 0; i < tokens.length; i++) {
+    tokens[i].draw();
+  }
+}
+
+function removeToken() {
+  background(43, 45, 57);
+  console.log(tokens.length);
+  if (tokens.length > 0) {
+    tokens.pop();
+    console.log(tokens.length);
+    for (let i = 0; i < tokens.length; i++) {
+      tokens[i].draw();
+    }
+  }
 }
